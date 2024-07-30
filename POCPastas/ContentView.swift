7380fa0d6e27bd78c8a30de11528a2acd6930d5c
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2024-07-30 15:23:55 -0300

    Initial Commit

---
+++ POCPastas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POCPastas
+//
+//  Created by Gabriel Vicentin Negro on 30/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POCPastas/POCPastasApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCPastasApp.swift
+//  POCPastas
+//
+//  Created by Gabriel Vicentin Negro on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct POCPastasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
